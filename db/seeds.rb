a48f6fd43b859d6c23ef7e2362b39ffaf76039df
Simon Leiva <leivaprogramming@gmail.com>
2020-05-14 16:12:43 -0400

    my database > your database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+ingredient_list = JSON.parse(user_serialized)
+
+
+ingredient_list['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient.values)
+end
